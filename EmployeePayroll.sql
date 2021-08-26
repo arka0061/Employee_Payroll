499c5179e852222b4e9470d220e7b152667fc5b7
Arka <arka.spectacular@gmail.com>
2021-08-26 15:40:58 +0530

    [Arka]Add:Retrieve Selective Data by using Cast and GetDate and where

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -9,8 +9,11 @@ Salary varchar(20),
 StartDate varchar(20)
 )
 --UC3 Create Employee Payroll Data
-insert into employee_payroll (Name,Salary,StartDate)values ('arka','50000','26-08-2021')
-insert into employee_payroll (Name,Salary,StartDate)values ('rahul','30000','26-08-2021')
-insert into employee_payroll (Name,Salary,StartDate)values ('rahul','45000','26-08-2021')
+insert into employee_payroll (Name,Salary,StartDate)values ('arka','50000','2021-05-20')
+insert into employee_payroll (Name,Salary,StartDate)values ('rahul','30000','2021-02-20')
+insert into employee_payroll (Name,Salary,StartDate)values ('rahul','45000','2021-03-20')
 --UC4 Retrieve Data
 select * from employee_payroll
+--UC5 Selective Data Retrieval
+select * from employee_payroll where Name='rahul'
+select Salary from employee_payroll where startDate BETWEEN Cast('2021-03-20' as Date) and GetDate();
