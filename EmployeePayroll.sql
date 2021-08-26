01ef70c1b5b3951e8fb21f509d06928cd98e39e1
Arka <arka.spectacular@gmail.com>
2021-08-26 15:53:08 +0530

    [Arka]Add:Gender To Employee

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -12,8 +12,17 @@ StartDate varchar(20)
 insert into employee_payroll (Name,Salary,StartDate)values ('arka','50000','2021-05-20')
 insert into employee_payroll (Name,Salary,StartDate)values ('rahul','30000','2021-02-20')
 insert into employee_payroll (Name,Salary,StartDate)values ('rahul','45000','2021-03-20')
+insert into employee_payroll (Name,Salary,StartDate)values ('priyanka','60000','2021-07-20')
 --UC4 Retrieve Data
 select * from employee_payroll
 --UC5 Selective Data Retrieval
 select * from employee_payroll where Name='rahul'
 select Salary from employee_payroll where startDate BETWEEN Cast('2021-03-20' as Date) and GetDate();
+--UC6 Add Gender to Employee
+Alter table employee_payroll
+add Gender char(1)
+
+Update employee_payroll 
+set Gender='M' where name='arka' or name='rahul'
+Update employee_payroll 
+set Gender='F' where name='priyanka'
