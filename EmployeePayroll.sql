6ffa7061f71fa267556909a5b3b274a17391c5a1
Arka <arka.spectacular@gmail.com>
2021-08-27 20:54:47 +0530

    [Arka]Add:Aggregate Functions on EmployeePayroll Database items

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -26,3 +26,47 @@ Update employee_payroll
 set Gender='M' where name='arka' or name='rahul'
 Update employee_payroll 
 set Gender='F' where name='priyanka'
+
+/*Create DATABASE Employee
+use Employee
+
+Create Table Student(
+Id int identity(1,1) primary key,
+Nmae varchar(20),
+Email varchar(40)
+)
+
+Create Table Teacher(
+id int identity(1,1) primary key,
+Name varchar(20),
+Email varchar(40),
+StudentId int FOREIGN KEY REFERENCES Student(Id)
+)
+
+Create Table CR(
+id int identity(1,1) primary key,
+Name varchar(20),
+Email varchar(40),
+)*/
+
+use payroll_service
+--UC7 Aggregrate Functions
+Alter Table employee_payroll
+Alter Column Salary money
+Select SUM(Salary) from employee_payroll Where Gender='M' Group by Gender
+Select SUM(Salary) from employee_payroll Where Gender='F' Group by Gender
+
+Select AVG(Salary) from employee_payroll Where Gender='M' Group by Gender
+Select AVG(Salary) from employee_payroll Where Gender='F' Group by Gender
+
+Select MAX(Salary) from employee_payroll Where Gender='M' Group BY Gender
+Select MAX(Salary) from employee_payroll Where Gender='F' Group BY Gender
+
+Select MIN(Salary) from employee_payroll Where Gender='M' Group BY Gender
+Select MIN(Salary) from employee_payroll Where Gender='F' Group BY Gender
+
+Select COUNT(Salary) from employee_payroll Where Gender='M' Group BY Gender
+Select COUNT(Salary) from employee_payroll Where Gender='F' Group BY Gender
+
+
+
